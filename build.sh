4fe427a92e6cf0e3e49436ccb340713b9dec2f26
Lesley <laisililai@live.cn>
2016-05-07 10:38:21 +0800

    Remove customize /src since openshift connot support it

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Builds .org files
-echo "============================================================="
-echo "Building org files:"
-echo "============================================================="
-emacs -batch -l org_export.el -kill
--- src/about_en.md
+++
@@ -1,8 +0,0 @@
----
-layout: page
-title: About
-permalink: /About/
-ref: about
-lang: en
----
-
