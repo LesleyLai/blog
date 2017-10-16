765630a5dd9e8349025b44f8566531151f027e5c
Lesley Lai <laisililai@live.cn>
2017-10-16 13:49:42 -0600

    Change header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,19 +6,20 @@ import { MenuProps } from "../menu"
 
 const NavBar = ({ items, pathname }: MenuProps) => (
     <nav>
-        <Menu.Item as="a" className="mobile only" icon="sidebar" />
+        <Menu as="nav" secondary size="large">
 
-        <Menu size="large" className="mobile hidden">
+            <Menu.Item as="a" className="mobile only" icon="sidebar" />
 
             {items.map((item) => {
-                const active: boolean = (item.exact) ? pathname === item.path : pathname.startsWith(item.path);
-                return <Menu.Item
-                    as={Link}
-                    name={item.name}
-                    to={item.path}
-                    key={item.path}
-                    active={active}
-                />;
+                 const active: boolean = (item.exact) ? pathname === item.path : pathname.startsWith(item.path);
+                 return <Menu.Item
+                     as={Link}
+                     name={item.name}
+                     to={item.path}
+                     key={item.path}
+                     active={active}
+                     className="mobile hidden"
+                 />;
             })}
         </Menu>
     </nav >
@@ -37,22 +38,19 @@ const Header = ({ items, pathname }: MenuProps) => (
                 margin: '0 auto',
                 maxWidth: 960,
                 padding: '0.3rem 1.0875rem',
+                display: 'flex',
             }}
         >
-            <h1 style={{
-                margin: 0,
-                display: 'inline'
-            }}>
-                <Link
-                    to="/"
-                    style={{
-                        color: 'white',
-                        textDecoration: 'none',
-                    }}
-                >
-                    Lesley Lai
-                </Link>
-            </h1>
+            <Link
+                to="/"
+                style={{
+                    color: 'white',
+                    textDecoration: 'none',
+                    marginRight: '15px',
+                }}
+            >
+                <h1>Lesley Lai</h1>
+            </Link>
             <NavBar items={items} pathname={pathname} />
         </div>
     </header>
