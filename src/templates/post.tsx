0ca6506b2e8428a183da067e1fe9ae384b62ba55
Lesley Lai <laisililai@live.cn>
2018-03-18 21:45:10 -0600

    Update dependencies

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -1,40 +1,40 @@
 import * as React from "react";
 import TagsList from "../components/tags-list";
-//const ReactDisqusThread = require('react-disqus-thread');
+// import ReactDisqusComments from "react-disqus-comments";
 
 export default ({ data }) => {
-    const post = data.markdownRemark;
-    const path = '/' + post.frontmatter.id + '/' + post.frontmatter.lang + '/';
-    const url = "http://lesleylai.info" + path;
-    return (<div>
-        <h1>{post.frontmatter.title}</h1>
-        <TagsList tags={post.frontmatter.categories} />
-        <ul>
-            <li>Create: {post.frontmatter.create}</li>
-            <li>Last Modify: {post.frontmatter.lastModify}</li>
-        </ul>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        {/* <ReactDisqusThread
-            shortname="lesleylaiblog"
-            identifier={post.frontmatter.id}
-            title={post.frontmatter.title}
-            url={url}
-            onNewComment={this.handleNewComment} /> */}
-    </div>);
+  const post = data.markdownRemark;
+  const path = '/' + post.frontmatter.id + '/' + post.frontmatter.lang + '/';
+  const url = "http://lesleylai.info" + path;
+  return (<div>
+    <h1>{post.frontmatter.title}</h1>
+    <TagsList tags={post.frontmatter.categories} />
+    <ul>
+      <li>Create: {post.frontmatter.create}</li>
+      <li>Last Modify: {post.frontmatter.lastModify}</li>
+    </ul>
+    <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    {/*<ReactDisqusComments
+      shortname="lesleylaiblog"
+      identifier={post.frontmatter.id}
+      title={post.frontmatter.title}
+      url={url}
+      onNewComment={this.handleNewComment} />*/}
+  </div>);
 };
 
 export const query = graphql`
-    query BlogPostQuery($relativePath: String!) {
-        markdownRemark(fields: { relativePath: { eq: $relativePath } }) {
-            html
-            frontmatter {
-                id
-                lang
-                title
-                create(formatString: "DD MMMM, YYYY")
-                lastModify(formatString: "DD MMMM, YYYY")
-                categories
-            }
-        }
+  query BlogPostQuery($relativePath: String!) {
+    markdownRemark(fields: { relativePath: { eq: $relativePath } }) {
+      html
+      frontmatter {
+        id
+        lang
+        title
+        create(formatString: "DD MMMM, YYYY")
+        lastModify(formatString: "DD MMMM, YYYY")
+        categories
+      }
     }
+  }
 `;
