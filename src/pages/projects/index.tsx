9b903d3f03773559956f2f51866f7c02de7ba47b
Lesley Lai <laisililai@live.cn>
2018-04-03 21:14:56 -0600

    Projects page css design

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -7,36 +7,38 @@ interface ProjectProps extends React.HTMLProps<HTMLDListElement> {
 }
 
 const Project = (prop: ProjectProps) => {
+  const css = require("./project.module.css");
   return (
-    <li>
-      <a href={prop.url} > <b>{prop.name}</b> </a>
+    <div className={css.project}>
+      <h3><a href={prop.url} > <b>{prop.name}</b> </a></h3>
       <p>
         {prop.children}
       </p>
-    </li>
+    </div>
   )
 }
 
 const ProjectsPage = () => {
   const title = "My projects";
   const rayTracerImg = require("./raytracer.png");
+  const css = require("./projects.module.css");
 
   return (<div>
     <Helmet>
       <title>{"Lesley Lai | " + title}</title>
     </Helmet>
     <h1>{title}</h1>
-    <ul>
+    <ul className={css.projectsList}>
       <Project
         name="Reversi-Elm"
         url="https://github.com/LesleyLai/Reversi-Elm">
         A purely functional implementation of the classic board game Reversi. You can play the game <a href="https://lesleylai.github.io/Reversi-Elm/">here</a>.
-      </Project>
+        </Project>
       <Project
         name="Ray tracer"
         url="https://github.com/LesleyLai/RayTracer">
         A recursive <a href="https://en.wikipedia.org/wiki/Ray_tracing_(graphics)">ray tracer</a> in C++. It can handle ray-sphere and ray-triangle intersection.
-        <img src={rayTracerImg} alt="Ray tracer" />
+          <img src={rayTracerImg} alt="Ray tracer" />
       </Project>
     </ul>
   </div>
