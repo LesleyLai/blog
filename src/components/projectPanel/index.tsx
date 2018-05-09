8ea2b84d9f99a0326f9bf313db8d688f1d215f46
Lesley Lai <laisililai@live.cn>
2018-05-08 19:19:49 -0600

    Add two projects I did

--- src/components/projectPanel/index.tsx
+++ src/components/projectPanel/index.tsx
@@ -5,19 +5,31 @@ const css = require("./projectPanel.module.css");
 interface ProjectPenalProps extends React.HTMLProps<HTMLDListElement> {
   name: string;
   url: string;
+
+  // A reference to image
   image?: string;
+  // Prevent the panel to show only part of the image, but the image may not fill the panel this way
+  imageContain?: boolean;
+
+  // Year of doing that project
+  year: string;
 }
 
 const ProjectPanel = (props: ProjectPenalProps) => {
   return (
     <article className={css.panel}>
-      <ProjectPanelHeader url={props.url} image={props.image} />
+      <ProjectPanelHeader
+        url={props.url}
+        image={props.image}
+        imageContain={props.imageContain}
+      />
 
       <div className={css.content}>
         <h3>
           <a className={css.name} href={props.url}>
             {props.name}
-          </a>
+          </a>{" "}
+          ({props.year})
         </h3>
         {props.children}
       </div>
@@ -29,6 +41,7 @@ const ProjectPanel = (props: ProjectPenalProps) => {
 interface HeaderProps extends React.HTMLProps<HTMLDListElement> {
   url: string;
   image?: string;
+  imageContain?: boolean;
 }
 
 const ProjectPanelHeader = (props: HeaderProps) => {
@@ -38,7 +51,8 @@ const ProjectPanelHeader = (props: HeaderProps) => {
       return {
         backgroundImage: `url(${image})`,
         backgroundPosition: "center",
-        backgroundSize: "cover"
+        backgroundSize: props.imageContain ? "contain" : "cover",
+        backgroundRepeat: "no-repeat"
       };
     } else {
       return {};
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -9,6 +9,8 @@ const ProjectsPage = () => {
 
   const reversiImage = require("./reversi.png");
   const raytracerImage = require("./raytracer.png");
+  const pathTracingImage = require("./pathtracing.png");
+  const tetrisImage = require("./tetris.png");
 
   return (
     <div>
@@ -18,24 +20,56 @@ const ProjectsPage = () => {
       <h1>{title}</h1>
       <ul className={css.projectsList}>
         <ProjectPanel
+          name="Path tracer"
+          url="https://github.com/LesleyLai/PathTracer"
+          image={pathTracingImage}
+          year="2018"
+        >
+          A Monte-Carlo method based path tracing program for my own learning
+          purpose. Loosely based on Peter Shirley's{" "}
+          <a href="http://in1weekend.blogspot.lt/">
+            Ray Tracing in One Weekend
+          </a>{" "}
+          mini book and its sequels.
+        </ProjectPanel>
+
+        <ProjectPanel
           name="Reversi-Elm"
           url="https://github.com/LesleyLai/Reversi-Elm"
           image={reversiImage}
+          year="2018"
         >
           A purely functional implementation of the classic board game Reversi.
           You can play the game{" "}
           <a href="https://lesleylai.github.io/Reversi-Elm/">here</a>.
         </ProjectPanel>
+
+        <ProjectPanel
+          name="Tetris"
+          url="https://github.com/LesleyLai/Tetris"
+          image={tetrisImage}
+          imageContain
+          year="2017"
+        >
+          A simple Tetris clone in Python and{" "}
+          <a href="https://en.wikipedia.org/wiki/Tk_(software)">Tk</a> library.
+        </ProjectPanel>
+
         <ProjectPanel
           name="Ray tracer"
           url="https://github.com/LesleyLai/RayTracer"
           image={raytracerImage}
+          year="2016"
         >
-          A recursive{" "}
+          This is a toy{" "}
           <a href="https://en.wikipedia.org/wiki/Ray_tracing_(graphics)">
             ray tracer
           </a>{" "}
-          in C++. It can handle ray-sphere and ray-triangle intersection.
+          in C++ for the Edx's{" "}
+          <a href="https://www.edx.org/course/computer-graphics-uc-san-diegox-cse167x-3">
+            Computer Graphics
+          </a>{" "}
+          Course. It can handle ray-sphere and ray-triangle intersection.
         </ProjectPanel>
       </ul>
     </div>
