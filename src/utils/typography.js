6b578362dd9fac19c3819680352ffe4a5237bcea
Lesley <lesleylai@lesleylai.info>
2019-11-04 15:59:22 -0700

    Fix some issues with chinese font

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,25 @@
 import Typography from "typography";
 const typography = new Typography({
   baseFontSize: "16px",
-  bodyFontFamily: ["Lato", "georgia", "serif"]
+  bodyFontFamily: [
+    "Lato",
+    "georgia",
+    "serif",
+    "Microsoft Yahei",
+    "微软雅黑",
+    "STXihei",
+    "华文细黑"
+  ],
+  headerFontFamily: [
+    "Lato",
+    "georgia",
+    "serif",
+    "sans-serif",
+    "Microsoft Yahei",
+    "微软雅黑",
+    "STXihei",
+    "华文细黑"
+  ]
 });
 
 export default typography;
