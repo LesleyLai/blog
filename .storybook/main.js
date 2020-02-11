59fb95a903a32034f1a76ce2cfc70490640d0594
Lesley Lai <lesley@lesleylai.info>
2020-02-10 20:15:11 -0700

    Add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.tsx'],
+  addons: [
+    "@storybook/addon-a11y",
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-viewport",
+    "@storybook/addon-knobs",
+  ],
+};
