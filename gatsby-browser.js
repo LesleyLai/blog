32544dbad8b3cde13f8fb8459f0adfeee2a47a0b
Lesley <lesleylai@lesleylai.info>
2020-09-29 13:25:03 -0600

    Fix some bugs in shouldUpdateScroll

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,10 +7,12 @@ require("typeface-noto-sans-sc");
 const languages = require("./src/utils/translations").languages;
 
 exports.shouldUpdateScroll = ({
-    prevRouterProps: { location: previousLocation },
+    prevRouterProps,
     routerProps: { location },
     getSavedScrollPosition
   }) => {
+      if (!prevRouterProps) return true;
+      const previousLocation = prevRouterProps.location;
       for (lang of languages) {
         const regex = new RegExp(`\/${lang}\/[0-9]+`, "g");
 
@@ -22,7 +24,7 @@ exports.shouldUpdateScroll = ({
 
         // Return false if the transition is a pagination link
         if ((isPaginationPage(location.pathname) && isPaginationPage(previousLocation.pathname))) {
-            return "pagination";
+          return "pagination";
         };
       }
 
