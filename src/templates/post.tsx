c617300993ccc264352030ff306a925f777bf621
Lesley <lesleylai@lesleylai.info>
2020-10-17 05:15:42 -0600

    Add commento

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -1,5 +1,5 @@
 import { graphql } from "gatsby";
-import * as React from "react";
+import React, { useEffect } from "react";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import PrevNextLinks from "../components/prevnextlinks";
 import Socials from "../components/socials";
@@ -62,6 +62,41 @@ interface PostProps {
     };
   };
 }
+
+// Helper to add scripts to our page
+const insertScript = (src: string, id: string, parentElement: HTMLElement) => {
+  const script = window.document.createElement("script");
+  script.async = true;
+  script.src = src;
+  script.id = id;
+  parentElement.appendChild(script);
+  return script;
+};
+// Helper to remove scripts from our page
+const removeScript = (id: string, parentElement: HTMLElement) => {
+  const script = window.document.getElementById(id);
+  if (script) {
+    parentElement.removeChild(script);
+  }
+};
+// The actual component
+const Commento = ({ id }: { id: string }) => {
+  useEffect(() => {
+    // If there's no window there's nothing to do for us
+    if (!window) {
+      return;
+    }
+    const document = window.document;
+    // In case our #commento container exists we can add our commento script
+    if (document.getElementById("commento")) {
+      insertScript(`https://cdn.commento.io/js/commento.js`, `commento-script`, document.body);
+    }
+    // Cleanup; remove the script from the page
+    return () => removeScript(`commento-script`, document.body);
+  }, [id]);
+
+  return <div id={`commento`} />;
+};
 
 class PostTemplate extends React.Component<PostProps> {
   public handleNewComment() {
@@ -128,7 +163,9 @@ class PostTemplate extends React.Component<PostProps> {
 
           <PrevNextLinks lang={lang} previousInfo={previousInfo} nextInfo={nextInfo} />
 
-          <div className={css.comment}>{/* For comment */}</div>
+          <div className={css.comment}>
+            <Commento id={path} />
+          </div>
         </div>
       </Layout>
     );
