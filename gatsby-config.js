3a29cfe538d0fca37d6bb215fb2f4fd1114d3672
Lesley Lai <lesley@lesleylai.info>
2021-01-06 03:13:16 -0700

    Adds gatsby-remark-copy-linked-files (for svg)

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,7 +114,13 @@ module.exports = {
               // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
               strict: `ignore`
             }
-          }
+          },
+          {
+            resolve: `gatsby-remark-copy-linked-files`,
+            options: {
+              ignoreFileExtensions: [`png`, `jpg`, `jpeg`, `bmp`, `tiff`],
+            },
+          },
         ]
       }
     },
