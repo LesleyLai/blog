5ce20a5d30927cc2ccce4fb0fc45924c6e056174
Lesley Lai <lesley@lesleylai.info>
2024-04-09 03:53:13 +0800

    A Language type

---
+++ src/i18n/i18n.ts
@@ -0,0 +1,5 @@
+export type Language = "en" | "zh";
+
+export const languages = ["en", "zh"] as Language[];
+
+export const isLanguage = (str: string): str is Language => (languages as string[]).includes(str);
